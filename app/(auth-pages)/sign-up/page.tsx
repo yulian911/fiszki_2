5fe14df3170276fb1489245d1c64daaeb49a8060
yulian911 <julian95@o2.pl>
2025-06-15 11:02:05 +0200

    vercel update

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -1,12 +1,15 @@
 import SignUpForm from "./SignUpForm";
 import { SmtpMessage } from "../smtp-message";
+import { Suspense } from "react";
 
 export default function Signup() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="flex flex-col gap-4">
-        <SignUpForm />
-        <SmtpMessage />
+        <Suspense>
+          <SignUpForm />
+          <SmtpMessage />
+        </Suspense>
       </div>
     </div>
   );
